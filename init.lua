c4fefdcadb32b867ef97264e73c15c7702412141
Ben Lokash <ben.lokash@curv.ai>
2024-03-06 13:56:33 -0500

    add prettier for js, ts

--- init.lua
+++ init.lua
@@ -671,7 +671,10 @@ require('lazy').setup {
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
       },
     },
   },
